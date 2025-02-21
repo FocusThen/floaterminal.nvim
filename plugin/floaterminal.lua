533fe38e61184e774a53fc76accef2b53a7fd1ea
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-02-21 00:45:00 +0000

    my dumb first plugin
    ty tjdev

---
+++ plugin/floaterminal.lua
@@ -0,0 +1,50 @@
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+local state = {
+  floating = {
+    buf = -1,
+    win = -1
+  }
+}
+
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'editor', -- Relative to the entire editor
+    row = row,           -- Vertical position
+    col = col,           -- Horizontal position
+    width = width,       -- Window width
+    height = height,     -- Window height
+    border = 'rounded',  -- Window border style
+    style = 'minimal',   -- No status line, no number line
+  })
+
+  return { win = win, buf = buf }
+end
+
+local toggle_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window { buf = state.floating.buf }
+    if vim.bo[state.floating.buf].buftype ~= "terminal" then
+      vim.cmd.term()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end
+
+vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
+vim.keymap.set({ "n", "t" }, "<leader>tt", toggle_terminal)
